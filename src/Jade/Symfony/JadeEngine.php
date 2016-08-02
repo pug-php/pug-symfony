a163fda0a6de43684ab9aefacb2fb212c6b7743a
kylekatarnls <kylekatarnls@gmail.com>
2016-08-02 18:01:56 +0200

    Cleanup

--- src/Jade/Symfony/JadeEngine.php
+++ src/Jade/Symfony/JadeEngine.php
@@ -6,20 +6,5 @@ use Jade\Jade;
 
 class JadeEngine extends Jade
 {
-    /**
-     * Compile PHP code from a Pug input or a Pug file.
-     *
-     * @param string input
-     *
-     * @throws \Exception
-     *
-     * @return string
-     */
-    public function compile($input)
-    {
-        $php = parent::compile($input);
-        $php = preg_replace('/(\\Jade\\Compiler::getPropertyFromAnything\((?:[^()]++|(?R))*+\))\(((?:[^()]++|(?R))*+)\)/', 'call_user_func($1, $2)', $php);
-
-        return $php;
-    }
+    // Jade engine customization come here.
 }
