6f05016b6cd77e63a0ae964f36e3d906ffc31f18
kylekatarnls <kylekatarnls@gmail.com>
2016-08-04 11:58:01 +0200

    assets:publish command

--- README.md
+++ README.md
@@ -43,3 +43,20 @@ public function contactAction()
     ]);
 }
 ```
+
+## Deployment
+
+In production, you better have to pre-render all your templates to improve performances. To do that, you have to add Pug\PugSymfonyBundle\PugSymfonyBundle in your registered bundles.
+
+In **app/AppKernel.php**, in the ```registerBundles()``` method, add the Pug bundle:
+```php
+public function registerBundles()
+{
+    $bundles = [
+        ...
+        new Pug\PugSymfonyBundle\PugSymfonyBundle(),
+    ];
+```
+
+This will make the ```assets:publish``` command available, now each time you deploy your app, enter the command below:
+```php bin/console assets:publish --env=prod```
---
+++ src/Jade/JadeSymfonyBundle/Command/AssetsPublishCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Jade\JadeSymfonyBundle\Command;
+
+use Jade\Jade;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AssetsPublishCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('assets:publish')
+            ->setDescription('Export your assets in the web directory.')
+        ;
+    }
+
+    protected function cacheTemplates(Jade $pug)
+    {
+        $success = 0;
+        $errors = 0;
+        $directories = array();
+        foreach ($pug->getOption('assetDirectory') as $assetDirectory) {
+            $viewDirectory = $assetDirectory . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'views';
+            if (!is_dir($viewDirectory)) {
+                continue;
+            }
+            $directories[] = $viewDirectory;
+            $data = $pug->cacheDirectory($viewDirectory);
+            $success += $data[0];
+            $errors += $data[1];
+        }
+
+        return array($directories, $success, $errors);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        list($directories, $success, $errors) = $this->cacheTemplates(
+            $this->getContainer()->get('templating.engine.pug')->getEngine()
+        );
+        $count = count($directories);
+        $output->writeln($count . ' ' . ($count === 1 ? 'directory' : 'directories') . ' scanned: ' . implode(', ', $directories) . '.');
+        $output->writeln($success . ' templates cached.');
+        $output->writeln($errors . ' templates failed to be cached.');
+    }
+}
