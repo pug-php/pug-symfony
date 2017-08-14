0c7dbd4220d04aed36cbab22f1abb3cec47ad6a8
Kyle <kylekatarnls@users.noreply.github.com>
2017-08-14 23:36:52 +0200

    Apply fixes from StyleCI (#18)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,8 @@
 include_once __DIR__ . '/../vendor/autoload.php';
 
 if (!class_exists('PHPUnit_Framework_TestCase')) {
-    class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase {
+    class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase
+    {
         // PHPUnit compatibility
     }
 }
