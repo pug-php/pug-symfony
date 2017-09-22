dd483e60fd1a43259c21bf8dd4d85ce6c1b6d9a7
KyleKatarn <kylekatarnls@gmail.com>
2017-09-22 14:50:27 +0200

    Fix coverage maximum version

--- tests/sendCoverage.php
+++ tests/sendCoverage.php
@@ -1,6 +1,6 @@
 <?php
 
-if (version_compare(PHP_VERSION, '5.6') >= 0 && version_compare(PHP_VERSION, '7.1') < 0) {
+if (version_compare(PHP_VERSION, '5.6') >= 0 && version_compare(PHP_VERSION, '7.0') < 0) {
     echo 'Send coverage report for ' . PHP_VERSION;
     chdir(__DIR__ . '/..');
     exec('vendor/bin/test-reporter --coverage-report coverage.xml', $output, $status);
