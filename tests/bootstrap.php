3e66ebf871cefc23af0ef816e1800b15ed769774
Gabriel Caruso <carusogabriel34@gmail.com>
2017-11-26 18:30:31 -0200

    Refactoring tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,10 +3,3 @@
 include_once __DIR__ . '/../vendor/autoload.php';
 include_once __DIR__ . '/project/src/TestBundle/TestBundle.php';
 include_once __DIR__ . '/project/app/AppKernel.php';
-
-if (!class_exists('PHPUnit_Framework_TestCase')) {
-    class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase
-    {
-        // PHPUnit compatibility
-    }
-}
