f7e97f3f3cc2b8202ef1b0cb698838364815a8fc
KyleKatarn <kylekatarnls@gmail.com>
2018-01-24 11:25:59 +0100

    Split Symfony dependencies

--- tests/setSymfonyVersion.php
+++ tests/setSymfonyVersion.php
@@ -1,35 +1,8 @@
 <?php
 
 list($pugVersion, $symfonyVersion) = explode(' ', implode(' ', array_slice($argv, 1)), 2);
-$composerFile = __DIR__ . '/../composer.json';
-$composer = file_get_contents($composerFile);
-$newContent = preg_replace('/"symfony\/symfony"\s*:\s*"[^"]+"/', '"symfony/symfony": "' . $symfonyVersion . '"', $composer);
 
-if ($newContent === $composer) {
-    echo 'symfony/symfony not found in composer.json';
-
-    exit(1);
-}
-
-echo "symfony/symfony version: $symfonyVersion\n";
-
-$composer = $newContent;
-$newContent = preg_replace('/"pug-php\/pug"\s*:\s*"[^"]+"/', '"pug-php/pug": "' . $pugVersion . '"', $composer);
-
-if ($newContent === $composer) {
-    echo 'pug-php/pug not found in composer.json';
-
-    exit(1);
-}
-
-echo "pug-php/pug version: $pugVersion\n";
-
-if (empty($newContent) || !file_put_contents($composerFile, $newContent)) {
-    echo 'composer.json cannot be updated';
-
-    exit(1);
-}
-
-echo 'composer.json has been updated';
+echo shell_exec('composer require --no-update -n symfony/symfony=' . $symfonyVersion);
+echo shell_exec('composer require --no-update -n pug-php/pug=' . $pugVersion);
 
 exit(0);
