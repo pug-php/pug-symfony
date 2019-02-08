6843b5a57367c5ef1228ff6a2ca9837f430c5ebe
Константин <onefivefournine@gmail.com>
2019-02-08 12:06:14 +0300

    inherit twig globals

--- src/Jade/JadeSymfonyEngine.php
+++ src/Jade/JadeSymfonyEngine.php
@@ -97,16 +97,10 @@ class JadeSymfonyEngine implements EngineInterface, InstallerInterface, HelpersH
         ], ($container->hasParameter('pug') ? $container->getParameter('pug') : null) ?: []));
         $this->registerHelpers($container, array_slice(func_get_args(), 1));
         $this->assets = new Assets($this->jade);
-        $app = new AppVariable();
-        $app->setDebug($kernel->isDebug());
-        $app->setEnvironment($environment);
-        $app->setRequestStack($container->get('request_stack'));
-        // @codeCoverageIgnoreStart
-        if ($container->has('security.token_storage')) {
-            $app->setTokenStorage($container->get('security.token_storage'));
+
+        foreach ($container->get('twig')->getGlobals() as $globalKey => $globalValue){
+            $this->share($globalKey, $globalValue);
         }
-        // @codeCoverageIgnoreEnd
-        $this->share('app', $app);
     }
 
     protected function getEngineClassName()
