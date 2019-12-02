1f39ace17699bdb82fe122fac0c4f47ec62898a9
KyleKatarn <kylekatarnls@gmail.com>
2019-12-02 15:11:32 +0100

    Fix class loading order

--- tests/Pug/FakeAssetsHelper.php
+++ tests/Pug/FakeAssetsHelper.php
@@ -4,7 +4,7 @@ namespace Symfony\Bundle\FrameworkBundle\Templating\Helper;
 
 class FakeAssetsHelper extends AssetsHelper
 {
-    public function getUrl($url, $packageName = null)
+    public function getUrl($url, $packageName = null, $version = null)
     {
         return "fake:$url";
     }
